8358977215c00ae4eeca7389990a7894e210aa71
Bryant Omoregie <unclebryant@gmail.com>
2020-03-04 20:46:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,7 @@ def load_library(file_path)
     new_hash["get_meaning"][value[1]] = key 
     new_hash["get_emoticon"][value[0]] = value[1] 
   end
-  binding.pry
+  #binding.pry
   new_hash
 end
 
