f3676cc0c20c8e9c842ee68d363fa3344899c298
Bryant Omoregie <unclebryant@gmail.com>
2020-03-04 21:20:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,8 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  load_library(file_path)
+  emoticons = load_library(file_path)
+  binding.pry 
 end
 
 def get_english_meaning
